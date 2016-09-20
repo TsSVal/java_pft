89609a3b5b0de8203d0def4f3016fbb51a7d2775
sofia tsunskaya <stsunskaya@scnsoft.com>
2016-09-20 16:19:41 +0300

    some functions were added

--- snadbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ snadbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,22 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 public static void main(String[] args) {
-System.out.println("Hello, World");
+hello ("World");
+    hello ("user");
+    hello ("sheep");
+    double len = 5;
+    System.out.println("square area with side " + len + " = " + area (len));
+    double a = 4;
+    double b = 6;
+    System.out.println("square of rectangular box with sides " + a + " and " + b + " = " + area (a, b));
+}
+public static void hello (String somebody) {
+    System.out.println("Hello, " + somebody + " !");
+}
+public static double area (double l) {
+   return  l * l;
+}
+public static double area (double a, double b) {
+    return a * b;
 }
 }
